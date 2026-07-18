1e1d880cd328755a39b598dfff9a9802d48c9dcb
akrhouseapartment-cloud <akrhouseapartment@gmail.com>
2026-07-19 01:47:12 +0530

    Create navigation.js

---
+++ js/navigation.js
@@ -0,0 +1,21 @@
+// AKR AMS Navigation
+
+function goToHome() {
+    window.location.href = "index.html";
+}
+
+function goToLogin() {
+    window.location.href = "login.html";
+}
+
+function goToRegister() {
+    window.location.href = "register.html";
+}
+
+function goToResident() {
+    window.location.href = "resident.html";
+}
+
+function goToAdmin() {
+    window.location.href = "admin.html";
+}
