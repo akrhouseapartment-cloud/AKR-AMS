15d4c15439c586297ec98d45f788dfeaf413cbd3
akrhouseapartment-cloud <akrhouseapartment@gmail.com>
2026-07-19 01:32:59 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// AKR AMS
+// Main Application Script
+
+const APP_NAME = "AKR House Apartment Management System";
+const VERSION = "1.0.0";
+
+console.log(APP_NAME);
+console.log("Version " + VERSION);
+
+function showMessage(message) {
+    alert(message);
+}
