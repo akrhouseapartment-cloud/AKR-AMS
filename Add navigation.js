db9047349685c0f9246b4b4c6951d49e2939a22c
akrhouseapartment-cloud <akrhouseapartment@gmail.com>
2026-07-19 01:48:44 +0530

    Create Add navigation.js

---
+++ Add navigation.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>AKR AMS</title>
+
+<link rel="stylesheet" href="css/style.css">
+<script src="js/navigation.js"></script>
+</head>
+
+<body>
+
+<div class="container">
+
+<h1>🏢 AKR AMS</h1>
+
+<h2>AKR House Apartment</h2>
+
+<button onclick="goToLogin()">Resident Login</button>
+
+<br><br>
+
+<button onclick="goToRegister()">Register</button>
+
+<br><br>
+
+<button onclick="goToAdmin()">Admin Login</button>
+
+</div>
+
+</body>
+</html>
